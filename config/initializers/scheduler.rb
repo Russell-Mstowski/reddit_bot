4664286fb6396d6651b8c3e57a4c16fa74ed5a63
Russell Mstowski <rmstow2@uic.edu>
2017-12-29 18:46:53 -0600

    first commit

---
+++ config/initializers/scheduler.rb
@@ -0,0 +1,35 @@
+require 'unirest'
+require 'twitter'
+require 'rufus-scheduler'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['API_KEY']
+  config.consumer_secret     = ENV['API_SEC']
+  config.access_token        = ENV['ACC_TOK']
+  config.access_token_secret = ENV['ACC_TOK_SEC']
+end
+
+scheduler = Rufus::Scheduler.new
+
+last_post = {:id => ""}
+count = 0
+scheduler.every '50s' do
+  response = Unirest.get("https://www.reddit.com/r/CryptoCurrency/new/.json")
+
+  posts = response.body["data"]["children"]
+
+  newest_post = posts.first["data"]
+
+  tweet = "New post in r/CryptoCurrency: #{newest_post["title"]} + https://www.reddit.com/#{newest_post["permalink"]} #crypto"
+
+  if newest_post["id"] != last_post["id"]
+    client.update(tweet)
+    puts "#{count}: I tweeted!!!!!!!!! + #{newest_post["title"]}"
+  end
+
+  last_post = newest_post
+  count += 1
+end
+
+
+

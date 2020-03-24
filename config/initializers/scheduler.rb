e01c1310f0ffdf2981814ed1259461a447092c69
Russell Mstowski <russell.mstowski@gmail.com>
2020-03-23 20:52:40 -0500

    update scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -13,26 +13,25 @@ scheduler = Rufus::Scheduler.new
 refresher = Rufus::Scheduler.new
 
 last_post = {:id => ""}
-count = 0
 scheduler.every '1h' do
   response = Unirest.get("https://www.reddit.com/r/CryptoCurrency/hot/.json")
 
   posts = response.body["data"]["children"]
 
+  posts.delete_if {|post| post["title"].includes? 'Daily Discussion' }
+
   newest_post = posts.shuffle.first["data"]
 
-  tweet = "#{newest_post["title"]} + https://www.reddit.com#{newest_post["permalink"]} #cryptocurrency #crypto #blockchain"
+  tweet = "#{newest_post["title"]} + #{newest_post["url"]} #bitcoin #cryptocurrency #crypto"
 
   if newest_post["id"] != last_post["id"]
     client.update(tweet)
-    puts "#{count}: I tweeted!!!!!!!!! + #{newest_post["title"]}"
   end
 
   last_post = newest_post
-  count += 1
 end
 
-refresher.every '10m' do
+refresher.every '25m' do
   Unirest.get('https://redditcryptobot.herokuapp.com')
   puts "refreshed!!"
 end
